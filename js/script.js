976535d7629cb110e80c800fe3e13f8c2b05d037
Alexey <killwii.arch@gmail.com>
2020-03-17 23:57:01 +0300

    simple lines

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+'use strict';
+
+let size = 10.5;
+
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(windowWidth, windowHeight);
+    // noLoop();
+}
+
+function draw() {
+    background(30);
+    fill(240);
+    strokeWeight(0.5);
+    stroke(240);
+    line(mouseX, 0, mouseX, mouseY - size);
+    line(mouseX, mouseY + size, mouseX, height);
+
+    line(0, mouseY, mouseX - size, mouseY);
+    line(mouseX + size, mouseY, width, mouseY);
+
+
+    noFill();
+    strokeWeight(0.5);
+    ellipse(mouseX, mouseY, size * 2, size * 2);
+}
+
+function mousePressed() {
+    
+    redraw(4);
   }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+
